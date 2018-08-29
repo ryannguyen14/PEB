4dd1a65e859df4e054dac096c353f31852963598
Ryan Nguyen <rnwin7@gmail.com>
2018-08-29 10:11:18 -0400

    test

---
+++ Module0/VectorStatistics.m
@@ -0,0 +1,15 @@
+function [Mean, STD, Median] = VectorStatistics(v)
+    %{
+    Description:
+        Function that calculates the mean, standard deviation, and median.
+    
+    Inputs:
+        v:         vector of arbitrary length
+    
+    Outputs:
+        Mean:      mean of vector
+        STD:       standard deviation of vector
+        Median:    median of vector
+    
+    %}
+    
