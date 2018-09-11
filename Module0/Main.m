8bf4c28114e675aad4ebb558d4090e5614059361
Ryan Nguyen <ryan.nguyen@yale.edu>
2018-09-10 20:32:18 -0400

    Plotted MSD successfully I think

--- Module0/Main.m
+++ Module0/Main.m
@@ -2,7 +2,7 @@ clear;
 clc; 
 
 cd /Users/ryannguyen/Desktop/PEB/Module0
-
+%% 
 fileList = dir('*.csv'); 
 datax_1 = csvread(fileList(1).name);
 datax_2 = csvread(fileList(2).name);
@@ -11,6 +11,7 @@ datay_1 = csvread(fileList(4).name);
 datay_2 = csvread(fileList(5).name);
 datay_3 = csvread(fileList(6).name);
 
+%%
 time = datax_1(:,1);
 
 x(:,:,1) = datax_1(:,2:end);
@@ -20,19 +21,24 @@ x(:,:,3) = datax_3(:,2:end);
 y(:,:,1) = datay_1(:,2:end);
 y(:,:,2) = datay_2(:,2:end);
 y(:,:,3) = datay_3(:,2:end);
-
-%inds = round(logspace(time));
+%%
 N_t = length(time); 
 inds = unique(round(logspace(0, log(N_t)/log(10), 200)));
-delta_t = time(inds(2:end)) - time(inds(1:end-1));
+delta_t = time(inds(2:end)); %- time(inds(1:end-1));
 
-N_deltat = size(delta_t);
+N_deltat = length(delta_t);
 MSD = zeros(N_deltat, 3);
-
+%%
 for i = 1:length(inds) 
     ind = inds(i);
     dxs = x(1:end-ind,:,:) - x(1+ind:end,:,:); 
-    dys = y(1:end-ind,:,:) - y(1+ind:end,:,:); 
-    MSD(i,:) = mean(dxs^2 + dys^2,2); 
+    dys = y(1:end-ind,:,:) - y(1+ind:end,:,:);
+
+    MSD(i,:) = mean(mean(dxs.^2,2),1) + mean(mean(dys.^2,2),1);
 end 
-    
+%%
+loglog(inds,MSD(:,1));
+hold on;     
+loglog(inds,MSD(:,2));
+hold on;     
+loglog(inds,MSD(:,3));
